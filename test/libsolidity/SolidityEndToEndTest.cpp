064231d7cec72f469ca6b55e1766d98c8023748b
Christian Parpart <christian@ethereum.org>
2018-06-28 15:21:12 +0200

    This test seems to be obsolete.
    FYI: the `var g = x.mul` line is a function of type delegatecall, that
    has no representation in Solidity Language (?). Please correct me (hence, seperate commit).

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -8317,26 +8317,6 @@ BOOST_AUTO_TEST_CASE(using_for_by_name)
 	ABI_CHECK(callContractFunction("x()"), encodeArgs(u256(6 * 7)));
 }
 
-BOOST_AUTO_TEST_CASE(bound_function_in_var)
-{
-	char const* sourceCode = R"(
-		library D { struct s { uint a; } function mul(s storage self, uint x) returns (uint) { return self.a *= x; } }
-		contract C {
-			using D for D.s;
-			D.s public x;
-			function f(uint a) returns (uint) {
-				x.a = 6;
-				var g = x.mul;
-				return g({x: a});
-			}
-		}
-	)";
-	compileAndRun(sourceCode, 0, "D");
-	compileAndRun(sourceCode, 0, "C", bytes(), map<string, Address>{{"D", m_contractAddress}});
-	ABI_CHECK(callContractFunction("f(uint256)", u256(7)), encodeArgs(u256(6 * 7)));
-	ABI_CHECK(callContractFunction("x()"), encodeArgs(u256(6 * 7)));
-}
-
 BOOST_AUTO_TEST_CASE(bound_function_to_string)
 {
 	char const* sourceCode = R"(
